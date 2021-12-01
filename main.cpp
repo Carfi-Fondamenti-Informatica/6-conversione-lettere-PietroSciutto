f2f453cf25f8f1b62f8b3eb7c8dbaa31362b0a74
PietroSciutto <91790426+PietroSciutto@users.noreply.github.com>
2021-12-01 21:51:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
 int main(){
+  char a=a;
+    cin>>a;
+    if(conversione (a)==false){
+        cout<<"errore"<<endl;
+    }else{
+        cout<<a<<endl;
+    }
   return 0;
 }
