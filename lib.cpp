17f7cba0fbc540139d99e8ddfeb4edab6daa804f
PietroSciutto <91790426+PietroSciutto@users.noreply.github.com>
2021-12-01 22:14:52 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -5,7 +5,7 @@ bool conversione (char &a){
     if(a>=65 and a<=90 or a>=97 and a<=122){
         t=true;
         if(a>=65 and a<=90){
-            a=a+32;
+            a=a-32;
             return &a;
         }
     }else{
